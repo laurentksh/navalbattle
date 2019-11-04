4c1dacba86a0efb948e3eee6c7a015ec095f5748
Luca Landry <lucalandry@icloud.com>
2019-11-04 12:21:39 +0100

    Models and controller

---
+++ BatailleNavale/BatailleNavale/Controller.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BatailleNavale
+{
+    class Controller
+    {
+        List<ModelBateau> bateaux = new List<ModelBateau>();
+        public void createBateau (int x, int y, int size, ModelBateau.Orientation orientation)
+        {
+            ModelBateau bateau = new ModelBateau(x, y, size, orientation);
+            bateaux.Add(bateau);
+        }
+    }
+}
---
+++ BatailleNavale/BatailleNavale/ModelGrid.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BatailleNavale
+{
+    class ModelGrid
+    {
+        private int size { get; set; }
+        private List<ModelBateau> bateaux { get; set; }
+        private List<int> coups { get; set; }
+
+        public ModelGrid(int size, List<ModelBateau> bateaux, List<int> coups)
+        {
+            this.size = size;
+            this.bateaux = bateaux;
+            this.coups = coups;
+        }
+    }
+}
