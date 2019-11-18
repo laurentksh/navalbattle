aeefd2d92dd5d64a18b9ffeec181c521edfa35f6
Laurent Keusch <25180858+laurentksh@users.noreply.github.com>
2019-11-18 12:20:17 +0100

    Fixed IAController.

--- BatailleNavale/BatailleNavaleTest/Controller/IAControllerTest.cs
+++ BatailleNavale/BatailleNavaleTest/Controller/IAControllerTest.cs
@@ -7,8 +7,15 @@ namespace BatailleNavaleTest.Controllers
     [TestClass]
     public class IAControllerTest
     {
-        static SingleplayerGameController singleplayerGameController = new SingleplayerGameController(BatailleNavale.Model.IAModel.Difficulty.None);
-        IAController controller = new IAController(singleplayerGameController, BatailleNavale.Model.IAModel.Difficulty.None);
+        SingleplayerGameController singleplayerGameController;
+        IAController controller;
+
+        public IAControllerTest()
+        {
+
+            singleplayerGameController = new SingleplayerGameController(BatailleNavale.Model.IAModel.Difficulty.None);
+            controller = new IAController(singleplayerGameController, BatailleNavale.Model.IAModel.Difficulty.None);
+        }
 
         [TestMethod]
         public void GetNextTargetTest()
