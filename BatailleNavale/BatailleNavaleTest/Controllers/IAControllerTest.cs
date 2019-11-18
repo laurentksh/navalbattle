24f016bb69d6e104b79820abe065017930027455
Luca Landry <lucalandry@icloud.com>
2019-11-18 10:37:30 +0100

    Test Controllers

---
+++ BatailleNavale/BatailleNavaleTest/Controllers/IAControllerTest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using BatailleNavale.Controller;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BatailleNavaleTest.Controllers
+{
+    [TestClass]
+    public class IAControllerTest
+    {
+        static GameController GameController;
+        IAController controller = new IAController(GameController, BatailleNavale.Model.IAModel.Difficulty.None);
+
+        [TestMethod]
+        public void GetNextTargetTest()
+        {
+            controller.GetNextTarget();
+        }
+    }
+}
